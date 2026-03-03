d88f23fcb8c9e93f7a2756ca13d4e6825c681dca
maikolm <maicolpuertomejia1@gmail.com>
2026-03-02 21:47:52 -0500

    videojuego

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,23 @@ void main() {
     Scanner teclado=new Scanner(System.in);
     int op;
 
+    System.out.println("""
+                MENU PERSONAJES 
+                1. MOSTRAR INFOMACION INCIAL
+                2. CREAR PERSONAJE
+                3. BUSCAR
+                4. HACER QUE GANE EXPERIENCIA
+                5. SUBIR NIVEL  
+                6. APLICAR DAÑO
+                7. CURARLO
+                8. SALIR
+                """);
+
+
+
+
     do{
+
     }
 
 
